85d31d8f1104304b9ac7c52cebcbec70163d3509
PeterSonne <papa@deep2chill.de>
2020-03-05 14:12:03 +0800

    Songs done

--- src/pages/Songs.js
+++ src/pages/Songs.js
@@ -1,37 +1,54 @@
-import React from 'react'
-import axios from 'axios'
+import React from "react";
+import axios from "axios";
 
-import '../styles/songs.css'
+import "../styles/songs.css";
 
-import Sidebar from '../components/Sidebar'
-import Song from '../components/Song'
+import Sidebar from "../components/Sidebar";
+import Song from "../components/Song";
 
 class Songs extends React.Component {
-	state = {
-		songs: []
-	}
-	componentWillMount() {
-		axios
-			.get(``)
-			.then(res => {
-				this.setState({})
-			})
-			.catch(err => {
-				console.log({ err })
-			})
-	}
-	render() {
-		return (
-			<div id="page">
-				<Sidebar page="songs" />
-				<div id="songs">
-					<table>
-						{/* songs */}
-					</table>
-				</div>
-			</div>
-		)
-	}
+  state = {
+    songs: []
+  };
+  componentWillMount() {
+    axios
+      .get(`${process.env.REACT_APP_API}/songs`)
+      .then(res => {
+        if (res.status == 200) {
+          let songs = res.data;
+          this.setState({ songs });
+        } else {
+        }
+      })
+      .catch(err => {
+        console.log({ err });
+      });
+  }
+  render() {
+    return (
+      <div id="page">
+        <Sidebar page="songs" />
+        <div id="songs">
+          <table>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Artist</th>
+                <th>Album</th>
+                <th>Genre</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.songs.map(e => (
+                <Song song={e} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Songs
+export default Songs;
